8cf7739fc143993b3c12c5c740d4273f3c483810
manelpz <manelpz@icloud.com>
2016-06-19 23:27:41 -0500

    update

---
+++ assets/Correct scrollspy example - JSFiddle Archivos/hotjar-13981.js
@@ -0,0 +1,10 @@
+window.hjSiteSettings = window.hjSiteSettings || {"testers_widgets":[],"surveys":[],"heatmaps":[],"recording_capture_keystrokes":true,"polls":[],"site_id":13981,"record_targeting_rules":[],"forms":[],"record":false,"r":0.0753553423,"deferred_page_contents":[]};
+
+window.hjBootstrap = window.hjBootstrap || function (scriptUrl) {
+    var s = document.createElement('script');
+    s.src = scriptUrl;
+    document.getElementsByTagName('head')[0].appendChild(s);
+    window.hjBootstrap = function() {};
+};
+
+hjBootstrap('https://script.hotjar.com/modules-e12a7ede294d22ffa3a8a711155156e3.js');
