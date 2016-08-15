d9ab5a0fe84b4d02d6620d50ccfceafc25bfaaf9
Emmanuel López <manelpz@icloud.com>
2016-08-14 20:22:17 -0500

    Update mail.php

--- mail.php
+++ mail.php
@@ -8,7 +8,7 @@ $mail->CharSet = "UTF-8";
 $mail->isSMTP();                            // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';             // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                     // Enable SMTP authentication
-$mail->Username = 'autosteamsa@gmail.com';          // SMTP username
+$mail->Username = // SMTP username
 $mail->Password = // SMTP password
 $mail->SMTPSecure = 'tls';                  // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 587;                          // TCP port to connect to
